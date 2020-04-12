4ceda8e43bc25595d4d071716374eb21e600aa4e
Chxprogrammer@Camille <camillerak05@gmail.com>
2020-04-12 13:17:35 +0300

    Design index.php + navbar (non fonctionnel) + calculatrice

--- index.php
+++ index.php
@@ -1,17 +1,131 @@
 <?php 
-include 'database.php';
+include 'database.php'; 
+/* A ajouter après avir fini le design 
 if(!isset($_SESSION['mdp'])){
 	header('Location: login.php');
 }
-
+*/
 ?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Le réseau social pour les profs de Mathématiques !</title>
-	<meta charset="utf-8">
+	<!-- include les éléments de head -->
+	<?php include 'head.php'; ?>
+	<!-- fin include les éléments de head -->
 </head>
 <body>
+	<!-- include la barre de navigation -->
+	<?php include 'navbar.php'; ?>
+	<!-- fin include la barre de navigation -->
+	<!-- Saut de ligne pour bien placer les éléments de la page -->
+	<br/><br/><br/><br/>
+	<!-- Saut de ligne pour bien placer les éléments de la page -->
+	<!-- Colonnes -->
+	<div class="container">
+		<!-- Message du haut pour créer un cours -->
+		<div class="alert alert-danger alert-dismissible fade show" role="alert">
+			Créez votre propre cours dès maintenant : <a href="" style="text-decoration: none; color: #000"><b><i class="fa fa-play" aria-hidden="true"></i>  Commencer !</b></a>
+  			<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    		<span aria-hidden="true">&times;</span>
+  			</button>
+		</div>
+		<!-- Fin Message du haut pour créer un cours -->
+	  	<div class="row">
+		    <!-- Première colonne (Principale = la plus grande) -->
+		    <div class="col-md col-xs-12 col-lg">
+		    	<!-- Publier une ressource pédago -->
+		    	<div class="card">
+		    		<div class="card-body">
+		    			<div class="row">
+			    			<div class="col-md-7 col-xs-12"><img src="images/female-user-testpng" height="60"> Publier des ressources pédagogiques !</div>
+	    					<div class="col-xs-12 col-md-4"><button type="button" class="btn btn-danger" data-toggle="modal" data-target="#exampleModal">Publier une ressource mathématique</button></div>
+			    		</div>
+		    		</div>
+		    	</div>
+		    	<!-- Fin Publier une ressource pédago -->
+		    	<br/>
+		    	<div class="card">
+		    		<div class="card-body">
 
+		    			<!-- Publications -->
+		    			<div class="card">
+		    				<div class="card-header">
+			    				<div class="row">
+			    						<div class=".col-"><img src="images/user-test.png" height="60" class="pdpAccueil"></div>
+	    								<div class="col-10"><span class="userInfoAccueil">Louis Quatorze <br/>
+										<i class="fa fa-users" aria-hidden="true"></i>
+	    								<span style="font-size: 10px;">
+	    								Dans le groupe : MathouMata
+	    								</span></span></div>
+			    				</div>
+		    				</div>
+		    				<div class="card-body">
+			    				<b>Ma ressource pédagogique... 
+			    				<br/>Une pomme vaut 2 euros, combien valent 10 pommes ? 
+			    				<br/>Aide : utiliser la règle de trois.</b>
+
+			    				<br/><br/>
+
+			    				<span class="tagsAccueilPubli"><i class="fa fa-file" aria-hidden="true"></i>  Classe 3ème, Maths, Proportionalité, Exercice, Cours, Autres critères</span>
+
+			    				<hr/>
+			    				<!-- Bouton ça m'intéresse -->
+    							<button type="button" class="btn btn" id="interresseButtonAccueil"><i class="fa fa-star-o" aria-hidden="true"></i>  Intéressé(e)</button>
+    							<!-- Fin Bouton ça m'intéresse -->
+		    				
+		    				</div>
+		    			</div>
+		    		</div>
+		    	</div><br/>
+		    	<!-- Fin des publications -->
+		    </div>
+		    <!-- Fin première colonne -->
+
+		    <!-- Deuxième colonne -->
+		    <div class="col-md-4 col-xs-12 col-lg-4">
+		    	<div class="card" id="activitiesAccueil">
+		    		<div class="card-body">
+		    			<h5 class="DeuxiemeColonneAccueilTitre"><i class="fa fa-bullhorn" aria-hidden="true"></i>   Dernières activités</h5>
+		    			<hr/>
+		    			<b><i class="fa fa-calculator" aria-hidden="true"></i>  Daniel Legrand a publié une ressources pédagogique dans le groupe</b> LesMathsLove<br/><br/>
+		    			<b><i class="fa fa-calculator" aria-hidden="true"></i>  Dupont Perette a publié une ressources pédagogique dans le groupe</b> ChatDoreMaths<br/><br/>
+		    			<b><i class="fa fa-calculator" aria-hidden="true"></i>  Pierre Leloup a publié une ressources pédagogique dans le groupe</b> ClaVieLesMahts<br/><br/>
+		    		</div>
+		    	</div><br/>
+		      
+		    </div>
+		    <!-- Fin deuxième colonne -->
+
+
+  		</div>
+	</div>
+	<!-- Fin Colonnes -->
+
+
+
+
+
+	<!-- Modal publier ressources-->
+	<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	  <div class="modal-dialog" role="document">
+	    <div class="modal-content">
+	      <div class="modal-header">
+	        <h5 class="modal-title" id="exampleModalLabel">Modal à configurer (Pas fini)</h5>
+	        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+	          <span aria-hidden="true">&times;</span>
+	        </button>
+	      </div>
+	      <div class="modal-body">
+	        ...
+	      </div>
+	      <div class="modal-footer">
+	        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+	        <button type="button" class="btn btn-primary">Save changes</button>
+	      </div>
+	    </div>
+	  </div>
+	</div>
+	<!-- Fin Modal publier ressources-->
 </body>
 </html>
---
+++ navbar.php
@@ -0,0 +1,195 @@
+<!-- Navbar fixed : navbar navbar-inverse navbar-fixed-top hidden-print -->
+<nav class="navbar navbar-expand-lg navbar-light fixed-top">
+  <a class="navbar-brand" href="index.php"><img src="images/logo-example.jpg" style="height: 45px; border-radius: 100%;"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#" style="color: white;"><i class="fa fa-sign-out" aria-hidden="true"></i>  Se déconnecter</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#" style="color: white;"><i class="fa fa-user" aria-hidden="true"></i>  Mon espace de travail</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#" style="color: white; background-color: #c0392b;"><i class="fa fa-file-pdf-o" aria-hidden="true"></i>  Créer un cours</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#" style="color: white;" data-toggle="modal" data-target="#Calculatrice"><i class="fa fa-calculator" aria-hidden="true"></i>  Calculatrice</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<!-- Fin navbar fixed -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- Calculatrice -->
+
+
+
+
+
+<style type="text/css">
+  .calculatrice
+{
+        width:300px;
+        height:300px;
+        background-color:#eeeeee;
+        border:2px solid #CCCCCC;
+        margin:auto;
+        padding-left:5px;
+        padding-bottom:5px;
+}
+.calculatrice td
+{
+        height:16.66%;
+}
+.calc_td_resultat
+{
+        text-align:center;
+}
+.calc_resultat
+{
+        width:90%;
+        text-align:right;
+}
+.calc_td_calculs
+{
+        text-align:center;
+}
+.calc_calculs
+{
+        width:90%;
+        text-align:left;
+}
+.calc_td_btn
+{
+        width:25%;
+        height:100%;
+}
+.calc_btn
+{
+        width:90%;
+        height:90%;
+        font-size:20px;
+}
+</style>
+
+
+
+
+<script type="text/javascript" src="calculate.js"></script>
+<div class="modal fade" id="Calculatrice" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Utilisez la calculatrice</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <table class="calculatrice" id="calc">
+            <tr>
+                <td colspan="4" class="calc_td_resultat">
+                    <input type="text" readonly="readonly" name="calc_resultat" id="calc_resultat" class="calc_resultat" onkeydown="javascript:key_detect_calc('calc',event);" />
+                </td>
+            </tr>
+            <tr>
+                <td class="calc_td_btn">
+                        <input type="button" class="calc_btn" value="CE" onclick="javascript:f_calc('calc','ce');" />
+                </td>
+                <td class="calc_td_btn">
+                        <input type="button" class="calc_btn" value="&larr;" onclick="javascript:f_calc('calc','nbs');" />
+                </td>
+                <td class="calc_td_btn">
+                        <input type="button" class="calc_btn" value="%" onclick="javascript:f_calc('calc','%');" />
+                </td>
+                <td class="calc_td_btn">
+                        <input type="button" class="calc_btn" value="+" onclick="javascript:f_calc('calc','+');" />
+                </td>
+            </tr>
+            <tr>
+                <td class="calc_td_btn">
+                        <input type="button" class="calc_btn" value="7" onclick="javascript:add_calc('calc',7);" />
+                </td>
+                <td class="calc_td_btn">
+                        <input type="button" class="calc_btn" value="8" onclick="javascript:add_calc('calc',8);" />
+                </td>
+                <td class="calc_td_btn">
+                        <input type="button" class="calc_btn" value="9" onclick="javascript:add_calc('calc',9);" />
+                </td>
+                <td class="calc_td_btn">
+                        <input type="button" class="calc_btn" value="-" onclick="javascript:f_calc('calc','-');" />
+                </td>
+            </tr>
+                        <tr>
+                <td class="calc_td_btn">
+                        <input type="button" class="calc_btn" value="4" onclick="javascript:add_calc('calc',4);" />
+                </td>
+                <td class="calc_td_btn">
+                        <input type="button" class="calc_btn" value="5" onclick="javascript:add_calc('calc',5);" />
+                </td>
+                <td class="calc_td_btn">
+                        <input type="button" class="calc_btn" value="6" onclick="javascript:add_calc('calc',6);" />
+                </td>
+                <td class="calc_td_btn">
+                        <input type="button" class="calc_btn" value="x" onclick="javascript:f_calc('calc','*');" />
+                </td>
+            </tr>
+            <tr>
+                <td class="calc_td_btn">
+                        <input type="button" class="calc_btn" value="1" onclick="javascript:add_calc('calc',1);" />
+                </td>
+                <td class="calc_td_btn">
+                        <input type="button" class="calc_btn" value="2" onclick="javascript:add_calc('calc',2);" />
+                </td>
+                <td class="calc_td_btn">
+                        <input type="button" class="calc_btn" value="3" onclick="javascript:add_calc('calc',3);" />
+                </td>
+                <td class="calc_td_btn">
+                        <input type="button" class="calc_btn" value="&divide;" onclick="javascript:f_calc('calc','/');" />
+                </td>
+            </tr>
+            <tr>
+                <td class="calc_td_btn">
+                        <input type="button" class="calc_btn" value="0" onclick="javascript:add_calc('calc',0);" />
+                </td>
+                <td class="calc_td_btn">
+                        <input type="button" class="calc_btn" value="&plusmn;" onclick="javascript:f_calc('calc','+-');" />
+                </td>
+                <td class="calc_td_btn">
+                        <input type="button" class="calc_btn" value="," onclick="javascript:add_calc('calc','.');" />
+                </td>
+                <td class="calc_td_btn">
+                        <input type="button" class="calc_btn" value="=" onclick="javascript:f_calc('calc','=');" />
+                </td>
+            </tr>
+        </table>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal" style="background-color: #ff7675;">Fermer</button>
+      </div>
+    </div>
+  </div>
+</div>
+        <script type="text/javascript">
+                document.getElementById('calc').onload=initialiser_calc('calc');
+        </script>
+
+<!-- Fin calculatrice -->
